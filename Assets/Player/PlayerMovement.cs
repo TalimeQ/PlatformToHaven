25916d3593f6a4fe869dad9d072f66584801743b
TalimeQ <boren1996@wp.pl>
2019-03-04 01:04:50 +0100

    #Early , decent looking map layout with collisions. Movement Implementation.

---
+++ Assets/Player/PlayerMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody2D playerRigidBodyRef;
+
+    //TODO :: Replace with player Scriptable ref so i can have everything in one neat place
+    [SerializeField]
+    float movementSpeed = 5.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+         playerRigidBodyRef = this.GetComponentInParent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical  = Input.GetAxis("Vertical");
+        ProcessMovementInput(horizontal, vertical);
+    }
+    void ProcessMovementInput(float horizontal, float vertical)
+    {
+     
+        Vector2 horizontalMovementVector = this.transform.right * horizontal * movementSpeed;
+        Vector2 verticalMovementVector = this.transform.up *  vertical * movementSpeed;
+        playerRigidBodyRef.velocity = verticalMovementVector + horizontalMovementVector;
+  
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        print("Object collided");
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        print("Object Triggered");
+    }
+
+}

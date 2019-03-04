4d8dc2f27b4f824c32bc493742d7ed5bb67fa142
TalimeQ <boren1996@wp.pl>
2019-03-04 15:01:00 +0100

    #Base Movement Animation

--- Assets/Player/PlayerMovement.cs
+++ Assets/Player/PlayerMovement.cs
@@ -9,11 +9,15 @@ public class PlayerMovement : MonoBehaviour
     //TODO :: Replace with player Scriptable ref so i can have everything in one neat place
     [SerializeField]
     float movementSpeed = 5.0f;
+    [SerializeField]
+    Animator playerAnimatorRef;
+    private float movement = 0;
 
     // Start is called before the first frame update
     void Start()
     {
          playerRigidBodyRef = this.GetComponentInParent<Rigidbody2D>();
+         playerAnimatorRef = this.GetComponentInParent<Animator>();
     }
 
     // Update is called once per frame
@@ -26,11 +30,12 @@ public class PlayerMovement : MonoBehaviour
     }
     void ProcessMovementInput(float horizontal, float vertical)
     {
-     
+        movement = horizontal + vertical;
+        playerAnimatorRef.SetFloat("movement", movement);
         Vector2 horizontalMovementVector = this.transform.right * horizontal * movementSpeed;
         Vector2 verticalMovementVector = this.transform.up *  vertical * movementSpeed;
         playerRigidBodyRef.velocity = verticalMovementVector + horizontalMovementVector;
-  
+        
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
